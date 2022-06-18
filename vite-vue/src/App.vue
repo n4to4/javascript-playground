e6976c1c763e51a5c73c9e447f38a8f5aa736b35
n4to4 <n4to4k@gmail.com>
2022-06-18 10:43:21 +0900

    vue

--- vite-vue/src/App.vue
+++ vite-vue/src/App.vue
@@ -1,12 +1,16 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+export default {
+    data() {
+        return {
+            message: "Hello Vue 3 + TypeScript + Vite"
+        }
+    }
+}
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <h1>{{ message }}</h1>
 </template>
 
 <style>
